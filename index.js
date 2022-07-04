d17bd5876baff9391f0d39418171260116ba69fe
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-04 13:54:48 +0000

    added route to post new data

--- index.js
+++ index.js
@@ -12,14 +12,51 @@ app.use(express.json());
 app.use(cors());
 
 async function main(){
-    let db = await MongoUtil.connect(MONGOURI, 'tattoo-artists')
+    let db = await MongoUtil.connect(MONGOURI, 'tattoo-api');
+
+    app.post('/add-new-artist', async function(req,res){
+        let name = req.body.name;
+        let gender = req.body.gender;
+        let yearStarted = req.body.yearStarted;
+        let method = req.body.method;
+        let temporary = req.body.temporary;
+        let style = req.body.style;
+        let ink = req.body.ink;
+        let contact = req.body.contact;
+        let studio = req.body.studio;
+        studio[0]['_id'] = new ObjectId()
+    
+        try{
+            let result = await db.collection('tattoo-artists').insertOne({
+                name: name,
+                gender: gender,
+                yearStarted: yearStarted,
+                method: method,
+                temporary: temporary,
+                style: style,
+                ink: ink,
+                contact: contact,
+                studio: studio,
+            });
+            res.status(200);
+            res.send(result);
+        } catch (e){
+            res.status(500);
+            res.send({
+                error: 'error adding data, please contact administrator'
+            });
+            console.log(e)
+        }
+    
+    })
 }
 
 main();
 
 app.get('/', function(req,res){
-    res.send('hello')
+    res.send('test')
 })
+
 
 app.listen(8888, function(){
     console.log('server started')
