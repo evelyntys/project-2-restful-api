9109c406433110bf845951670e2915b00633e14b
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-04 14:08:19 +0000

    added skeleton code for read, update and delete

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ app.use(express.json());
 
 app.use(cors());
 
-async function main(){
+async function main() {
     let db = await MongoUtil.connect(MONGOURI, 'tattoo-api');
 
-    app.post('/add-new-artist', async function(req,res){
+    // CREATE
+    app.post('/add-new-artist', async function (req, res) {
         let name = req.body.name;
         let gender = req.body.gender;
         let yearStarted = req.body.yearStarted;
@@ -25,8 +26,8 @@ async function main(){
         let contact = req.body.contact;
         let studio = req.body.studio;
         studio[0]['_id'] = new ObjectId()
-    
-        try{
+
+        try {
             let result = await db.collection('tattoo-artists').insertOne({
                 name: name,
                 gender: gender,
@@ -40,24 +41,83 @@ async function main(){
             });
             res.status(200);
             res.send(result);
-        } catch (e){
+        } catch (e) {
             res.status(500);
             res.send({
                 error: 'error adding data, please contact administrator'
             });
             console.log(e)
         }
-    
+
+    })
+
+    // READ
+    app.get('/show-artists', async function (req, res) {
+        let criteria = {};
+        let results = await db.collection('tattoo-artists').find(criteria).toArray();
+
+        res.status(200);
+        res.send(results)
+    })
+
+    // UPDATE
+    app.put('/tattoo-artist/:id', async function (req, res) {
+        let name = req.body.name;
+        let gender = req.body.gender;
+        let yearStarted = req.body.yearStarted;
+        let method = req.body.method;
+        let temporary = req.body.temporary;
+        let style = req.body.style;
+        let ink = req.body.ink;
+        let contact = req.body.contact;
+        let studio = req.body.studio;
+
+        try {
+            let result = await db.collection('tattoo-artists').updateOne({
+                _id: ObjectId(req.params.id)
+            },
+                {
+                    '$set': {
+                        name: name,
+                        gender: gender,
+                        yearStarted: yearStarted,
+                        method: method,
+                        temporary: temporary,
+                        style: style,
+                        ink: ink,
+                        contact: contact,
+                        studio: studio,
+                    }
+                });
+            res.status(200);
+            res.send(result);
+        } catch (e) {
+            res.status(500);
+            res.send({
+                error: 'error updating data, please contact administrator'
+            });
+            console.log(e)
+        }
+    })
+
+    app.delete('/tattoo-artist/:id', async function(req,res){
+        let results = await db.collection('tattoo-artists').remove({
+            _id: ObjectId(req.params.id)
+        });
+        res.status(200);
+        res.send({
+            message: 'entry successfully deleted'
+        })
     })
 }
 
 main();
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.send('test')
 })
 
 
-app.listen(8888, function(){
+app.listen(8888, function () {
     console.log('server started')
 })
